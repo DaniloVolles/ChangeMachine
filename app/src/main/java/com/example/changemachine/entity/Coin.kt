9eb8297f2fe3c50aee5225d34d579951eb86d3e0
Danilo Volles <danilo.volles@outlook.com>
2023-03-19 16:55:07 -0300

    Update Coin to a data class.

--- app/src/main/java/com/example/changemachine/entity/Coin.kt
+++ app/src/main/java/com/example/changemachine/entity/Coin.kt
@@ -1,6 +1,6 @@
 package com.example.changemachine.entity
 
-class Coin(private val value: Double, private var quantity: Int) {
+data class Coin(private val value: Double, private var quantity: Long) {
 
     fun coinIn() {
         quantity++
