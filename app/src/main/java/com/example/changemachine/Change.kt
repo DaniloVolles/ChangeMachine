fa1c045d2595b8cdfde90d40a0a0ed6042a3d9e0
Danilo Volles <danilo.volles@outlook.com>
2023-03-22 22:08:34 -0300

    Change activity implementation. Logic implementation.

---
+++ app/src/main/java/com/example/changemachine/Change.kt
@@ -0,0 +1,93 @@
+package com.example.changemachine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.changemachine.databinding.ActivityChangeBinding
+import com.example.changemachine.entity.*
+import com.google.firebase.firestore.FirebaseFirestore
+
+class Change : AppCompatActivity() {
+
+    private lateinit var binding: ActivityChangeBinding
+    private val coinRef = FirebaseFirestore.getInstance().collection("Coin")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change)
+
+        binding = ActivityChangeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.generateChange.setOnClickListener {
+
+            calculateChange()
+
+            coinRef.document("cent100")
+                .addSnapshotListener { document, _ ->
+                    if (document != null) {
+                        val quantity = document.getLong("quantity")
+                        binding.quantity100.text = quantity.toString()
+                    }
+                }
+
+            coinRef.document("cent050")
+                .addSnapshotListener { document, _ ->
+                    if (document != null) {
+                        val quantity = document.getLong("quantity")
+                        binding.quantity050.text = quantity.toString()
+                    }
+                }
+        }
+
+    }
+
+    private fun calculateChange() {
+
+        var quantity100: Long = 100
+        var quantity050: Long = 100
+        var quantity025: Long = 100
+        var quantity010: Long = 100
+        var quantity005: Long = 100
+        var quantity001: Long = 100
+
+        val providedValue = binding.providedValue.text.toString().toDouble()
+        val purchaseValue = binding.purchaseValue.text.toString().toDouble()
+
+        val change = providedValue - purchaseValue
+        var coinCounter = 0.00
+
+        if (change != 0.00) {
+
+            while (change > coinCounter) {
+
+                val pointer = change - coinCounter
+
+                while (pointer >= 1.00 && quantity100 != 0L) {
+                    coinCounter += 1.00
+                    quantity100--
+                }
+                while (pointer >= 0.50 && quantity050 != 0L) {
+                    coinCounter += 0.50
+                    quantity050--
+                }
+                while (pointer >= 0.25 && quantity025 != 0L) {
+                    coinCounter += 0.25
+                    quantity025--
+                }
+                while (pointer >= 0.10 && quantity010 != 0L) {
+                    coinCounter += 0.10
+                    quantity010--
+                }
+                while (pointer >= 0.05 && quantity005 != 0L) {
+                    coinCounter += 0.05
+                    quantity005--
+                }
+                while (pointer >= 0.01 && quantity001 != 0L) {
+                    coinCounter += 0.01
+                    quantity001--
+                }
+
+            }
+        }
+    }
+}
