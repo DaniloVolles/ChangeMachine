5ab20da063a86b1790a6a9c522f4a9ba803e6ce0
Danilo Volles <danilo.volles@outlook.com>
2023-03-22 22:07:55 -0300

    objects update. Quantity value exclusion.

--- app/src/main/java/com/example/changemachine/entity/objects.kt
+++ app/src/main/java/com/example/changemachine/entity/objects.kt
@@ -1,15 +1,8 @@
 package com.example.changemachine.entity
 
-val cent001 = Coin(0.01, 0)
-val cent005 = Coin(0.05, 0)
-val cent010 = Coin(0.10, 0)
-val cent025 = Coin(0.25, 0)
-val cent050 = Coin(0.50, 0)
-val cent100 = Coin(1.00, 0)
-
-val bknt002 = BankNote(002.00, 0)
-val bknt005 = BankNote(005.00, 0)
-val bknt010 = BankNote(010.00, 0)
-val bknt020 = BankNote(020.00, 0)
-val bknt050 = BankNote(050.00, 0)
-val bknt100 = BankNote(100.00, 0)
+val cent001 = Coin(0.01)
+val cent005 = Coin(0.05)
+val cent010 = Coin(0.10)
+val cent025 = Coin(0.25)
+val cent050 = Coin(0.50)
+val cent100 = Coin(1.00)
