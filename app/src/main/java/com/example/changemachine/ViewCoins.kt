d9820b93558563e16c9bcd8dc0bc8d025808047a
Danilo Volles <danilo.volles@outlook.com>
2023-03-22 22:03:45 -0300

    ViewCoins path update.

--- app/src/main/java/com/example/changemachine/ViewCoins.kt
+++ app/src/main/java/com/example/changemachine/ViewCoins.kt
@@ -2,15 +2,14 @@ package com.example.changemachine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.changemachine.databinding.ActivityFillCashDeskBinding
+import android.widget.TextView
 import com.example.changemachine.databinding.ActivityViewCoinsBinding
-import com.example.changemachine.ui.viewDBFunction
 import com.google.firebase.firestore.FirebaseFirestore
 
 class ViewCoins : AppCompatActivity() {
 
     private lateinit var binding: ActivityViewCoinsBinding
-    private val db = FirebaseFirestore.getInstance()
+    private val db = FirebaseFirestore.getInstance().collection("Coin")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,42 +19,42 @@ class ViewCoins : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.viewCoins.setOnClickListener {
-            db.collection("Coin").document("cent100")
+            db.document("cent100")
                 .addSnapshotListener { document, _ ->
                     if (document != null) {
                         val quantity = document.getLong("quantity")
                         binding.quantity100.text = quantity.toString()
                     }
                 }
-            db.collection("Coin").document("cent050")
+            db.document("cent050")
                 .addSnapshotListener { document, _ ->
                     if (document != null) {
                         val quantity = document.getLong("quantity")
                         binding.quantity050.text = quantity.toString()
                     }
                 }
-            db.collection("Coin").document("cent025")
+            db.document("cent025")
                 .addSnapshotListener { document, _ ->
                     if (document != null) {
                         val quantity = document.getLong("quantity")
                         binding.quantity025.text = quantity.toString()
                     }
                 }
-            db.collection("Coin").document("cent010")
+            db.document("cent010")
                 .addSnapshotListener { document, _ ->
                     if (document != null) {
                         val quantity = document.getLong("quantity")
                         binding.quantity010.text = quantity.toString()
                     }
                 }
-            db.collection("Coin").document("cent005")
+            db.document("cent005")
                 .addSnapshotListener { document, _ ->
                     if (document != null) {
                         val quantity = document.getLong("quantity")
                         binding.quantity005.text = quantity.toString()
                     }
                 }
-            db.collection("Coin").document("cent001")
+            db.document("cent001")
                 .addSnapshotListener { document, _ ->
                     if (document != null) {
                         val quantity = document.getLong("quantity")
